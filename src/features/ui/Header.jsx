c3da680596ec8fd30c9682b295775ba6904a09fe
SeunSarumi <seunsarumi@gmail.com>
2024-05-29 03:44:38 +0100

    fetching orders implemented

--- src/features/ui/Header.jsx
+++ src/features/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
 
       <p>Seun</p>
     </header>
