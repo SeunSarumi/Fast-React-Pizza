400dae02066c032b3b5195b3b3a9f849b0193272
SeunSarumi <seunsarumi@gmail.com>
2024-07-16 17:54:05 +0100

    cart page integrated with clear function

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -2,12 +2,12 @@ import { formatCurrency } from "../../utils/helpers";
 import Button from "../ui/Button";
 
 function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
+  const { pizzaId, name, quatity, totalPrice } = item;
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0 ">
-        {quantity}&times; {name}
+        {quatity}&times; {name}
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
