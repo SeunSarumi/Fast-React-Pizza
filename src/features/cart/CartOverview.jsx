2d970abe24e9788e60a163ac36dbd1790e3fbfd7
SeunSarumi <seunsarumi@gmail.com>
2024-07-16 17:23:20 +0100

    cart overview with redux selectors

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,20 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getTotalCartQuantity, getTotalCartPrice } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartPrice) return null;
+
   return (
     <div className="flex items-center justify-between p-4 px-4 py-4 text-sm uppercase text-stone-200 bg-stone-800 sm:px-4 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity} pizzas</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
